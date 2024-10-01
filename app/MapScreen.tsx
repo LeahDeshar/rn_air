b70af4058f29548c94bd48e8446a1f4aeea787f7
Leah Deshar <leah.desar@gmail.com>
2024-10-01 09:01:59 +0545

    init config

---
+++ app/MapScreen.tsx
@@ -0,0 +1,19 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MapScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        marginTop: 30,
+      }}
+    >
+      <Text>MapScreen</Text>
+    </View>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({});
